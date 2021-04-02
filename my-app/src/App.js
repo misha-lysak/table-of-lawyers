09a0153383d460556862a2141509bbe698156734
Misha Lysak <myhaillysak@gmail.com>
2021-04-02 16:08:14 +0300

    made thing, what u can upload own data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import Papa from 'papaparse';
-import data from './data.csv'
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
 import { Table } from './components/Table'
 import './App.scss';
 
 function App() {
-  const [rows, setRows] = useState([]);
   const [titles, setTitles] = useState([]);
-  const [incorrectFileError, setIncorrectFileError] = useState('');
-
+  const [rows, setRows] = useState([]);
   const titlesForValidation = titles.map(title => title.toLowerCase());
 
-  useEffect(() => {
-    async function getData() {
-      try {
-        const response = await fetch(data)
-        const reader = response.body.getReader()
-        const result = await reader.read()
-        const decoder = new TextDecoder('utf-8')
-        const csv = decoder.decode(result.value)
-        const results = Papa.parse(csv, { header: true })
-        const rows = results.data
-        setRows(rows)
-        setTitles(Object.keys(rows[0]))
-      } catch (error) {
-        setIncorrectFileError('Incorrect file. Choose csv file')
+  const processData = dataString => {
+    const dataStringLines = dataString.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+ 
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+
+      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+
+      if (headers && row.length == headers.length) {
+        const newObj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let td = row[j];
+          if (td.length > 0) {
+            if (td[0] == '"')
+              td = td.substring(1, td.length - 1);
+            if (td[td.length - 1] == '"')
+              td = td.substring(td.length - 2, 1);
+          }
+          if (headers[j]) {
+            newObj[headers[j]] = td;
+          }
+        }
+ 
+        if (Object.values(newObj).filter(x => x).length > 0) {
+          list.push(newObj);
+        }
       }
     }
-    getData()
-      .catch('adsfasd')
-  }, [])
+ 
+    setRows(list);
+    setTitles(headers);
+  }
+ 
+  const handleFileUpload = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      processData(data);
+    };
+
+    reader.readAsBinaryString(file);
+  }
+
   return (
     <>
-    {(titlesForValidation.includes('full name')
+    <input
+        className="file-input"
+        id="file"
+        type="file"
+        accept=".csv"
+        onChange={handleFileUpload}
+      />
+      <label htmlFor="file">Choose File</label>
+      {rows.length < 1 ? (
+        ''
+      ) :
+      (titlesForValidation.includes('full name')
       && titlesForValidation.includes('email')
       && titlesForValidation.includes('phone'))
       ? (
-      <div className="App">
-        {rows.length > 0 && (
-          <Table rows={rows} titles={titles} />
-        )}
-      </div>
-    ) : incorrectFileError.length > 0 ? (
-      <div className="error">{incorrectFileError}</div>
-    ) : (
-      <div className="error">{incorrectFileError}</div>
-
-    )}
+        <Table
+        titles={titles}
+        rows={rows}
+        />
+      ) : (<div className="error">Wrong data</div>)
+      }
     </>
   );
 }
